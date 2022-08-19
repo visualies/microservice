dd155cfcdab9751aa87f9c5fe235fd5afe1802ca
visualies <celentano.luc@googlemail.com>
2022-08-20 01:47:41 +0200

    Update

--- BaseService.Services/Services/ExampleService.cs
+++ BaseService.Services/Services/ExampleService.cs
@@ -11,26 +11,26 @@ namespace BaseService.Services.Services
 {
     public class ExampleService : IExampleService
     {
-        private readonly IUnitOfWork _unitOfWork;
+        private readonly IUnitOfWork _context;
 
         public ExampleService(IUnitOfWork unitOfWork)
         {
-            _unitOfWork = unitOfWork;
+            _context = unitOfWork;
         }
 
         public async Task<IEnumerable<Example>> FindAsync(ExampleRequest parameters)
         {
-            return await _unitOfWork.ExampleRepository.FindAsync(parameters);
+            return await _context.ExampleRepository.FindAsync(parameters);
         }
 
         public async Task<Example> GetAsync(ulong key)
         {
-            return await _unitOfWork.ExampleRepository.GetAsync(key);
+            return await _context.ExampleRepository.GetAsync(key);
         }
 
         public async Task CreateAsync(Example entity)
         {
-            await _unitOfWork.ExampleRepository.CreateAsync(entity);
+            await _context.ExampleRepository.CreateAsync(entity);
         }
 
         public Task DeleteAsync(ulong key)
